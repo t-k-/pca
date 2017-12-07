8d302f5255d753ddcc9d23c45425512bbd984b37
t-k- <clock126@126.com>
2017-12-07 16:45:11 -0500

    classification test.

--- test_class.m
+++ test_class.m
@@ -1,7 +1,10 @@
 clear
-nclass = 2;
-nexample = 1;
-
-[trainlabel, traindata] = readtrain(nclass, nexample);
-testdata = readtest(nclass, nexample);
-%y = linear_regression(X, [1 2 3 4], X)
+for nclass = [2, 3, 10]
+	for nexample = [1, 2]
+		[trainlabel, traindata] = readtrain(nclass, nexample);
+		testdata = readtest(nclass, nexample);
+		y = linear_regression(traindata, trainlabel, testdata);
+		[err, idx_wrong] = classerr_per(y, nclass, nexample);
+		[nclass, nexample, err]
+	end
+end
