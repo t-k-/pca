a4af6a29b86d3829443d4c57582b3e3f59fcdf1f
t-k- <clock126@126.com>
2017-12-08 14:52:52 -0500

    train, predict and judge accuracy using linear regression.

--- att_faces.m
+++ att_faces.m
@@ -19,32 +19,70 @@ method = 'eig';
 % % k = 20
 % [Y, k, meanX, P, Sigma, recX, sigmaPercents] = pca(dataset, 0.85, method);
 % % k = 68
-% [Y, k, meanX, P, Sigma, recX, sigmaPercents] = pca(dataset, 0.90, method);
-% % k = 111
+
+%%%%
+% Dimension reduction
+%%%%
+[Y, k, meanX, P, Sigma, recX, sigmaPercents] = pca(dataset, 0.90, method);
+% k = 111
+
+%%%%
+% top-4 recover images for different k
+%%%%
+figure;
+j = 1;
+all_k = [8, 20, 68, 111];
+for i = all_k
+	recoverX_i = pca_recover(dataset, meanX, P, i);
+	disp_face(recoverX_i, length(all_k), j);
+	title(sprintf('top-4 for k=%d', i));
+	j = j + 1;
+end
 
 
-% figure;
-% j = 1;
-% all_k = [8, 20, 68, 111];
-% for i = all_k
-% 	recoverX_i = pca_recover(dataset, meanX, P, i);
-% 	disp_face(recoverX_i, length(all_k), j);
-% 	title(sprintf('top-4 for k=%d', i));
-% 	j = j + 1;
-% end
+%%%%
+% extract training data and test data
+%%%%
+train_data = [];  % 111 by 280
+train_label = [];
+test_data = [];   % 111 by 120
+test_label = [];
 
-
-train_dat = []; % 111 by 280
-test_dat = [];  % 111 by 120
 cnt = 1;
 for subject = [1:40]
 	for i = [1:10]
 		reduced_img_vec = Y(:, cnt); % Y is 111 by n
 		if subject > 35 || i > 8
-			test_dat = horzcat(test_dat, reduced_img_vec);
+			test_data = horzcat(test_data, reduced_img_vec);
+			test_label = horzcat(test_label, subject);
 		else
-			train_dat = horzcat(train_dat, reduced_img_vec);
+			train_data = horzcat(train_data, reduced_img_vec);
+			train_label = horzcat(train_label, subject);
 		end
 		cnt = cnt + 1;
 	end
+end
+
+%%%%
+% train, predict and judge accuracy using affine_expansion
+%%%%
+[y, W] = linear_regression(train_data, @affine_expansion, train_label, test_data);
+err = test_label - y;
+err_rate = sum(err == 0) / length(err)
+
+%%%%
+% train, predict and judge accuracy using polynomial2_expansion
+%%%%
+[y, W] = linear_regression(train_data, @polynomial2_expansion, train_label, test_data);
+err = test_label - y;
+err_rate = sum(err == 0) / length(err)
+
+function Z = affine_expansion(X)
+	[h, w] = size(X);
+	Z = vertcat(ones(1, w), X);
+end
+
+function Z = polynomial2_expansion(X)
+	[h, w] = size(X);
+	Z = vertcat(ones(1, w), X, X(1,:).^2, X(2,:).^2);
 end
